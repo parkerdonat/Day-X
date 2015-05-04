8fa770f334ef6d9e0e427c608597c813471fb411
Kaleo <kauakim@gmail.com>
2015-05-04 16:56:25 -0600

    UITextarea

---
+++ DayX/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  DayX
+//
+//  Created by Kaleo Kim on 5/4/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
